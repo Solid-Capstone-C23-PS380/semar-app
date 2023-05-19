8d5cff5314d8c7b6dbc9f4350d684ab2aa63b192
Raixard <73803884+FerryFn@users.noreply.github.com>
2023-05-19 15:17:44 +0800

    FEAT: Downgrade Material theme version to comply with app mockup + Change bottom navigation item color to comply with app mockup + CHORE: Upgrade KotlinX's navigation fragment and UI version

--- app/src/main/java/com/solidcapstone/semar/MainActivity.kt
+++ app/src/main/java/com/solidcapstone/semar/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.solidcapstone.semar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.solidcapstone.semar.databinding.ActivityMainBinding
