73a78c8a6ef1c8abe4c44447ce79c42e9a5344cb
Bahrum <kaliber1933@gmail.com>
2023-05-20 18:45:40 +0700

    Update navigation for scanFragment

--- app/src/main/java/com/solidcapstone/semar/ui/event/EventFragment.kt
+++ app/src/main/java/com/solidcapstone/semar/ui/event/EventFragment.kt
@@ -1,13 +1,17 @@
 package com.solidcapstone.semar.ui.event
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import com.solidcapstone.semar.R
+import com.solidcapstone.semar.adapter.EventPagerAdapter
 import com.solidcapstone.semar.databinding.FragmentEventBinding
 import com.solidcapstone.semar.databinding.FragmentMissedEventBinding
+import java.util.Date
 
 
 class EventFragment : Fragment() {
@@ -22,6 +26,21 @@ class EventFragment : Fragment() {
         val root: View = binding.root
         return root
     }
+
+//    private fun setViewPager() {
+//        val pagerAdapter = EventPagerAdapter(Activity, date)
+//
+//        binding.apply {
+//            viewPager = viewpager
+//            tabs = tablayout
+//        }
+//
+//        viewPager.adapter = pagerAdapter
+//        val titles = resources.getStringArray(R.array.tab_menu)
+//        TabLayoutMediator(tabs, viewPager) { tab, position ->
+//            tab.text = titles[position]
+//        }.attach()
+//    }
 
 
 }
