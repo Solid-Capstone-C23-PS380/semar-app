aae552133838e0309a98239385a33ab15f1aa3a6
Bahrum <kaliber1933@gmail.com>
2023-05-20 14:17:50 +0700

    Wayang List Screen

---
+++ app/src/main/java/com/solidcapstone/semar/ui/home/ListWayangActivity.kt
@@ -0,0 +1,12 @@
+package com.solidcapstone.semar.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.solidcapstone.semar.R
+
+class ListWayangActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_wayang)
+    }
+}
