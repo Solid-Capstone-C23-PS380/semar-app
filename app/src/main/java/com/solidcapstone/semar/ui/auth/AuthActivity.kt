e93d954e166d544f264060d9d86a19ee5e4adc58
Bahrum <kaliber1933@gmail.com>
2023-05-22 22:00:28 +0700

    Update auth

--- app/src/main/java/com/solidcapstone/semar/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/solidcapstone/semar/ui/auth/AuthActivity.kt
@@ -3,10 +3,22 @@ package com.solidcapstone.semar.ui.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.solidcapstone.semar.R
+import com.solidcapstone.semar.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvToRegister.setOnClickListener{
+            clickToRegister()
+        }
+    }
+
+    private fun clickToRegister(){
+        RegisterFragment().show(supportFragmentManager, "RegisterFragment")
     }
 }
--- app/src/main/java/com/solidcapstone/semar/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/solidcapstone/semar/ui/auth/RegisterFragment.kt
@@ -1,5 +1,6 @@
 package com.solidcapstone.semar.ui.auth
 
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,18 +8,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.solidcapstone.semar.R
+import com.solidcapstone.semar.databinding.FragmentRegisterBinding
 
 
 class RegisterFragment : DialogFragment() {
 
+    private var _binding : FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+
+        // Mengatur lebar dan tinggi dialog
+        val width = resources.getDimensionPixelSize(R.dimen.dialog_width)
+        val height = resources.getDimensionPixelSize(R.dimen.dialog_height)
+        dialog.window?.setLayout(width, height)
+
+        return dialog
+    }
 
 }
