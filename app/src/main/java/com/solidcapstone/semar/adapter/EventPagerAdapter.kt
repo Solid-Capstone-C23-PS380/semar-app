8f4a902d9ba2590876afcb92d5825d1f510b2363
Raixard <73803884+FerryFn@users.noreply.github.com>
2023-05-25 13:06:47 +0800

    FEAT: Implement basic RecyclerView functionality in EventFragment

--- app/src/main/java/com/solidcapstone/semar/adapter/EventPagerAdapter.kt
+++ app/src/main/java/com/solidcapstone/semar/adapter/EventPagerAdapter.kt
@@ -5,15 +5,13 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.solidcapstone.semar.ui.event.ActiveEventFragment
 import com.solidcapstone.semar.ui.event.MissedEventFragment
-import java.util.Date
 
-class EventPagerAdapter(activity: AppCompatActivity, var date: Date) : FragmentStateAdapter(activity) {
-
+class EventPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
     override fun createFragment(position: Int): Fragment {
         var fragment: Fragment? = null
         when (position) {
-            0 -> fragment = MissedEventFragment(date)
-            1 -> fragment = ActiveEventFragment(date)
+            0 -> fragment = MissedEventFragment()
+            1 -> fragment = ActiveEventFragment()
         }
         return fragment as Fragment
     }
--- app/src/main/java/com/solidcapstone/semar/ui/event/EventFragment.kt
+++ app/src/main/java/com/solidcapstone/semar/ui/event/EventFragment.kt
@@ -1,46 +1,54 @@
 package com.solidcapstone.semar.ui.event
 
-import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.solidcapstone.semar.R
 import com.solidcapstone.semar.adapter.EventPagerAdapter
 import com.solidcapstone.semar.databinding.FragmentEventBinding
-import com.solidcapstone.semar.databinding.FragmentMissedEventBinding
-import java.util.Date
 
 
 class EventFragment : Fragment() {
+    private lateinit var binding: FragmentEventBinding
 
-    private var _binding: FragmentEventBinding? = null
-    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentEventBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+    ): View {
+        binding = FragmentEventBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-//    private fun setViewPager() {
-//        val pagerAdapter = EventPagerAdapter(Activity, date)
-//
-//        binding.apply {
-//            viewPager = viewpager
-//            tabs = tablayout
-//        }
-//
-//        viewPager.adapter = pagerAdapter
-//        val titles = resources.getStringArray(R.array.tab_menu)
-//        TabLayoutMediator(tabs, viewPager) { tab, position ->
-//            tab.text = titles[position]
-//        }.attach()
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setViewPager()
+    }
 
+    private fun setViewPager() {
+        val pagerAdapter = EventPagerAdapter(requireActivity() as AppCompatActivity)
 
+        val viewPager: ViewPager2 = binding.viewpager
+        viewPager.post { viewPager.setCurrentItem(1, false) }
+        viewPager.adapter = pagerAdapter
+
+        val tabs: TabLayout = binding.tabLayout
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+    }
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.event_tab_missed,
+            R.string.event_tab_active
+        )
+    }
 }
