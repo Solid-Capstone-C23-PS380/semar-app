fed1df62dab00ccff4b416c54a6206676aa2ff5c
Raixard <73803884+FerryFn@users.noreply.github.com>
2023-05-31 01:15:36 +0800

    FEAT: Hide navigation and status bar on video full screen

--- app/src/main/java/com/solidcapstone/semar/ui/detail/video/VideoDetailActivity.kt
+++ app/src/main/java/com/solidcapstone/semar/ui/detail/video/VideoDetailActivity.kt
@@ -2,11 +2,14 @@ package com.solidcapstone.semar.ui.detail.video
 
 import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.view.WindowInsetsController
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.FullscreenListener
@@ -26,7 +29,6 @@ class VideoDetailActivity : AppCompatActivity() {
     }
 
     private var videoIdYoutube: String? = null
-    private var videoIsFullscreen: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,39 +39,7 @@ class VideoDetailActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         showWayangDetail()
-        lifecycle.addObserver(binding.youtubePlayerView)
-
-        val iFramePlayerOptions: IFramePlayerOptions = IFramePlayerOptions.Builder()
-            .controls(1)
-            .fullscreen(1)
-            .build()
-
-        binding.apply {
-            youtubePlayerView.addFullscreenListener(object : FullscreenListener {
-                override fun onEnterFullscreen(fullscreenView: View, exitFullscreen: () -> Unit) {
-                    youtubePlayerView.visibility = View.GONE
-                    fullScreenViewContainer.visibility = View.VISIBLE
-                    fullScreenViewContainer.addView(fullscreenView)
-                    videoIsFullscreen = true
-                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
-                }
-
-                @SuppressLint("SourceLockedOrientationActivity")
-                override fun onExitFullscreen() {
-                    youtubePlayerView.visibility = View.VISIBLE
-                    fullScreenViewContainer.visibility = View.GONE
-                    fullScreenViewContainer.removeAllViews()
-                    videoIsFullscreen = false
-                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT
-                }
-            })
-
-            youtubePlayerView.initialize(object : AbstractYouTubePlayerListener() {
-                override fun onReady(youTubePlayer: YouTubePlayer) {
-                    youTubePlayer.loadVideo(videoIdYoutube ?: "dQw4w9WgXcQ", 0f)
-                }
-            }, iFramePlayerOptions)
-        }
+        initializeVideoPlayer()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -96,6 +66,64 @@ class VideoDetailActivity : AppCompatActivity() {
                 is Result.Error -> {
                     binding.pbVideoDetail.visibility = View.GONE
                 }
+            }
+        }
+    }
+
+    private fun initializeVideoPlayer() {
+        lifecycle.addObserver(binding.youtubePlayerView)
+
+        val iFramePlayerOptions: IFramePlayerOptions = IFramePlayerOptions.Builder()
+            .controls(1)
+            .fullscreen(1)
+            .build()
+
+        binding.apply {
+            youtubePlayerView.addFullscreenListener(object : FullscreenListener {
+                override fun onEnterFullscreen(fullscreenView: View, exitFullscreen: () -> Unit) {
+                    youtubePlayerView.visibility = View.GONE
+                    fullScreenViewContainer.visibility = View.VISIBLE
+                    fullScreenViewContainer.addView(fullscreenView)
+                    setSystemBarVisibility(false)
+                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+                }
+
+                @SuppressLint("SourceLockedOrientationActivity")
+                override fun onExitFullscreen() {
+                    youtubePlayerView.visibility = View.VISIBLE
+                    fullScreenViewContainer.visibility = View.GONE
+                    fullScreenViewContainer.removeAllViews()
+                    setSystemBarVisibility(true)
+                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT
+                }
+            })
+
+            youtubePlayerView.initialize(object : AbstractYouTubePlayerListener() {
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    youTubePlayer.loadVideo(videoIdYoutube ?: "dQw4w9WgXcQ", 0f)
+                }
+            }, iFramePlayerOptions)
+        }
+    }
+
+    private fun setSystemBarVisibility(isVisible: Boolean) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (isVisible) {
+                window.decorView.windowInsetsController?.show(WindowInsetsCompat.Type.systemBars())
+            } else {
+                window.decorView.windowInsetsController?.hide(WindowInsetsCompat.Type.systemBars())
+                window.decorView.windowInsetsController?.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            if (isVisible) {
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+            } else {
+                window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_FULLSCREEN
             }
         }
     }
